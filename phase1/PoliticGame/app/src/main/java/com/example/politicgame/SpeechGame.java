1c05a24c4bfdfc9f69e4bfcad58e85b64be2dace
zhaoyi80o <ytz.zhao@mail.utoronto.ca>
2019-10-26 14:45:12 -0400

    finished writing functionality for SpeechGame class

--- phase1/PoliticGame/app/src/main/java/com/example/politicgame/SpeechGame.java
+++ phase1/PoliticGame/app/src/main/java/com/example/politicgame/SpeechGame.java
@@ -1,6 +1,9 @@
 package com.example.politicgame;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class SpeechGame {
@@ -8,15 +11,51 @@ public class SpeechGame {
     private int points;
     private String input;
     private String prompt;
-    private ArrayList<String> associatedWords;
+    private String relevantWord;
 
-    public SpeechGame(){
+    private final int POINTSGIVEN = 1;
+    private final String PATH = "app\\assets\\SpeechPrompts.txt";
+
+    public SpeechGame() {
         this.points = 0;
         this.input = "";
         this.prompt = "";
-        this.associatedWords = new ArrayList<>();
+        this.relevantWord = "";
     }
 
+    private void readFile(String path){
+        File text = new File(path);
+        Scanner sc = new Scanner("");
+        try {
+            sc = new Scanner(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.prompt = sc.nextLine();
+        sc.useDelimiter(",");
+        while(sc.hasNext()){
+            String word = sc.next();
+            if (word.charAt(0) != '*') {
+                relevantWord = word;
+            }
+        }
+    }
 
+    public void setInput(String input) {
+        this.input = input.toLowerCase();
+    }
 
+    private void awardPoints(){
+        if (input.equals(relevantWord)){
+            this.points += POINTSGIVEN;
+        }
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
 }
