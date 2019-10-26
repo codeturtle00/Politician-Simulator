7b8acfe3c502699419e07f5f639efc3ef5832180
zhaoyi80o <ytz.zhao@mail.utoronto.ca>
2019-10-26 12:39:05 -0400

    made a new class for speech game objects

---
+++ phase1/PoliticGame/app/src/main/java/com/example/politicgame/SpeechGame.java
@@ -0,0 +1,22 @@
+package com.example.politicgame;
+
+import java.util.ArrayList;
+
+
+public class SpeechGame {
+
+    private int points;
+    private String input;
+    private String prompt;
+    private ArrayList<String> associatedWords;
+
+    public SpeechGame(){
+        this.points = 0;
+        this.input = "";
+        this.prompt = "";
+        this.associatedWords = new ArrayList<>();
+    }
+
+
+
+}
