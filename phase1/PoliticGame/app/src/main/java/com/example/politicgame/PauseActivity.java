26c2aea2ed137536d71b650c7881c67cf61a9002
Martin Chak <Martin.Chak@mail.utoronto.ca>
2019-10-27 15:26:33 -0400

    Added pause menu for StampGame

---
+++ phase1/PoliticGame/app/src/main/java/com/example/politicgame/PauseActivity.java
@@ -0,0 +1,22 @@
+package com.example.politicgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.politicgame.LeaderBoardActivity;
+import com.example.politicgame.R;
+
+public class PauseActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pause);
+
+
+    }
+}
