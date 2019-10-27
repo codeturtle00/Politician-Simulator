8a9a724ba751d7379dea1443a1e77fd863a09cea
Martin Chak <Martin.Chak@mail.utoronto.ca>
2019-10-27 16:50:07 -0400

    Added pause menus to each game

--- phase1/PoliticGame/app/src/main/java/com/example/politicgame/BabyGame/BabyActivity.java
+++ phase1/PoliticGame/app/src/main/java/com/example/politicgame/BabyGame/BabyActivity.java
@@ -2,11 +2,14 @@ package com.example.politicgame.BabyGame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.politicgame.MainActivity;
+import com.example.politicgame.PauseActivity;
 import com.example.politicgame.R;
 import com.example.politicgame.SpeechGame.SpeechInstructionActivity;
 
@@ -24,6 +27,17 @@ public class BabyActivity extends AppCompatActivity {
                         openSpeechGame();
                     }
                 });
+
+        final Button pauseB = findViewById(R.id.pause);
+        pauseB.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Log.i("Button", "The pause button has been clicked");
+
+                        //The method below will pause the game and handle the following inputs
+                        openPauseMenu();
+                    }
+                });
     }
 
     public void openSpeechGame() {
@@ -31,5 +45,38 @@ public class BabyActivity extends AppCompatActivity {
         startActivity(switchSpeechIntent);
     }
 
+    public void openPauseMenu(){
+        Intent pauseMenuIntent = new Intent(this, PauseActivity.class);
+        startActivityForResult(pauseMenuIntent, 1);
+    }
 
+    public void openMainMenu(){
+        Intent mainMenuIntent = new Intent(this, MainActivity.class);
+        startActivity(mainMenuIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //requestCode refers to the request code parameter of openPauseMenu's startActivityForResult call
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK){
+                int userInput = data.getIntExtra("result", 0);
+
+                if (userInput == 1){
+                    Log.i("Pause Result", "User has decided to resume play");
+                }
+
+                else if (userInput == 2){
+                    Log.i("Pause Result", "User has decided to quit the game");
+                    openMainMenu();
+                }
+            }
+
+            else {
+                Log.i("Result Code", "Result code is " + resultCode);
+            }
+        }
+    }
 }
--- phase1/PoliticGame/app/src/main/java/com/example/politicgame/SpeechGame/SpeechActivity.java
+++ phase1/PoliticGame/app/src/main/java/com/example/politicgame/SpeechGame/SpeechActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.politicgame.MainActivity;
+import com.example.politicgame.PauseActivity;
 import com.example.politicgame.R;
 import com.example.politicgame.StampGame.StampInstructionActivity;
 import java.io.BufferedReader;
@@ -41,6 +43,17 @@ public class SpeechActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_speech);
+
+    final Button pauseB = findViewById(R.id.pause);
+    pauseB.setOnClickListener(
+            new View.OnClickListener() {
+              public void onClick(View v) {
+                Log.i("Button", "The pause button has been clicked");
+
+                //The method below will pause the game and handle the following inputs
+                openPauseMenu();
+              }
+            });
   }
 
   @Override
@@ -176,6 +189,42 @@ public class SpeechActivity extends AppCompatActivity {
       // TODO: Keep point for the user
       failIntent.putExtra(INPUT_MESSAGE, userInput);
       startActivity(failIntent);
+    }
+  }
+
+
+  public void openPauseMenu(){
+    Intent pauseMenuIntent = new Intent(this, PauseActivity.class);
+    startActivityForResult(pauseMenuIntent, 1);
+  }
+
+  public void openMainMenu(){
+    Intent mainMenuIntent = new Intent(this, MainActivity.class);
+    startActivity(mainMenuIntent);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    super.onActivityResult(requestCode, resultCode, data);
+
+    //requestCode refers to the request code parameter of openPauseMenu's startActivityForResult call
+    if (requestCode == 1) {
+      if (resultCode == RESULT_OK){
+        int userInput = data.getIntExtra("result", 0);
+
+        if (userInput == 1){
+          Log.i("Pause Result", "User has decided to resume play");
+        }
+
+        else if (userInput == 2){
+          Log.i("Pause Result", "User has decided to quit the game");
+          openMainMenu();
+        }
+      }
+
+      else {
+        Log.i("Result Code", "Result code is " + resultCode);
+      }
     }
   }
 }
