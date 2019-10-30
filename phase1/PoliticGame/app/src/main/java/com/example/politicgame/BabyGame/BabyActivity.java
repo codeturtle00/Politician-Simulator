23fc129ddc7b37989cdff46a315a320dacd53ff7
Jacky Yang <yacky.yang@mail.utoronto.ca>
2019-10-30 18:19:26 -0400

    extend GameActivity

--- phase1/PoliticGame/app/src/main/java/com/example/politicgame/BabyGame/BabyActivity.java
+++ phase1/PoliticGame/app/src/main/java/com/example/politicgame/BabyGame/BabyActivity.java
@@ -11,32 +11,24 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.example.politicgame.GameActivity;
 import com.example.politicgame.MainActivity;
 import com.example.politicgame.PauseButton;
 import com.example.politicgame.PoliticGameApp;
 import com.example.politicgame.R;
 import com.example.politicgame.SpeechGame.SpeechInstructionActivity;
 
-public class BabyActivity extends AppCompatActivity implements BabyDraw {
+public class BabyActivity extends GameActivity implements BabyDraw {
   // Happiness of the baby. Also the player's score.
   private Integer happiness = 50;
 
   private TextView scoreDisplay;
   private TextView timer;
 
-  protected PoliticGameApp app;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    app = (PoliticGameApp) getApplication();
+  public void onCreate(Bundle savedInstanceState) {
 
-    System.out.println("The current theme is blue: " + app.isThemeBlue());
-
-    if (app.isThemeBlue()){
-      setTheme(R.style.BlueTheme);
-    } else {
-      setTheme(R.style.RedTheme);
-    }
 
     super.onCreate(savedInstanceState);
 
@@ -85,32 +77,5 @@ public class BabyActivity extends AppCompatActivity implements BabyDraw {
     happiness += happinessChange;
     String score = happiness.toString() + "%";
     scoreDisplay.setText(score);
-  }
-
-  public void openMainMenu() {
-    Intent mainMenuIntent = new Intent(this, MainActivity.class);
-    startActivity(mainMenuIntent);
-    finish();
-  }
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    // requestCode refers to the request code parameter of openPauseMenu's startActivityForResult
-    // call
-    if (requestCode == 1) {
-      if (resultCode == RESULT_OK) {
-        int userInput = data.getIntExtra("result", 0);
-
-        if (userInput == 1) {
-          Log.i("Pause Result", "User has decided to resume play");
-        } else if (userInput == 2) {
-          Log.i("Pause Result", "User has decided to quit the game");
-          openMainMenu();
-        }
-      } else {
-        Log.i("Result Code", "Result code is " + resultCode);
-      }
-    }
   }
 }
