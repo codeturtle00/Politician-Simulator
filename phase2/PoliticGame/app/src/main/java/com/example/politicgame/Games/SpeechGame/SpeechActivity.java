66c17dd88e1cd5bf7ec947856ba836a0e130d5ee
Yitian Zhao <ytz.zhao@mail.utoronto.ca>
2019-11-30 17:29:59 -0500

    Finished commenting for speech game

--- phase2/PoliticGame/app/src/main/java/com/example/politicgame/Games/SpeechGame/SpeechActivity.java
+++ phase2/PoliticGame/app/src/main/java/com/example/politicgame/Games/SpeechGame/SpeechActivity.java
@@ -23,6 +23,9 @@ public class SpeechActivity extends GameActivity {
     private SpeechPresenter presenter;
 
     @Override
+    /**
+     * Initializes the activity
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech);
@@ -32,6 +35,9 @@ public class SpeechActivity extends GameActivity {
         new PauseButton((ConstraintLayout) findViewById(R.id.speechLayout), this);
     }
 
+    /**
+     * Helper method to update the prompt and answers every time the user enters an input
+     */
     private void update(){
         ArrayList<String> choices = presenter.loadChoice();
         // TextView for prompt and choices
@@ -50,16 +56,19 @@ public class SpeechActivity extends GameActivity {
     }
 
     @Override
+    /**
+     * Initializes the SpeechPresenter object and regularly updates the prompts and answer choices
+     */
     protected void onStart() {
         super.onStart();
         presenter = (SpeechPresenter) this.getIntent().getSerializableExtra("SPEECH PRESENTER");
         update();
     }
 
-    /**
-     * This method is called when user click the compare button Compare the user input with the
-     * correct answer If the user input matches the answer, add points and go to successful result
-     * page If the user input does not match the answer,keep the point and go to fail result page
+    /**Compares the text of the user's input to the actual answer to decide which result to show
+     * the user.
+     *
+     * @param view
      */
     public void compare(View view) {
         EditText editText = findViewById(R.id.answer);
@@ -88,32 +97,12 @@ public class SpeechActivity extends GameActivity {
         }
     }
 
-
+    /**
+     * Goes back to the main menu
+     */
     public void openMainMenu() {
         Intent mainMenuIntent = new Intent(this, MainActivity.class);
         startActivity(mainMenuIntent);
         finish();
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //requestCode refers to the request code parameter of openPauseMenu's startActivityForResult call
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                int userInput = data.getIntExtra("result", 0);
-
-                if (userInput == 1) {
-                    Log.i("Pause Result", "User has decided to resume play");
-                } else if (userInput == 2) {
-                    Log.i("Pause Result", "User has decided to quit the game");
-                    openMainMenu();
-                }
-            } else {
-                Log.i("Result Code", "Result code is " + resultCode);
-            }
-        }
     }
 }
--- phase2/PoliticGame/app/src/main/java/com/example/politicgame/Games/SpeechGame/SuccessSpeechResult.java
+++ phase2/PoliticGame/app/src/main/java/com/example/politicgame/Games/SpeechGame/SuccessSpeechResult.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import com.example.politicgame.Application.PoliticGameApp;
 import com.example.politicgame.R;
 
+/**
+ * Inherits the SpeechResult activity, specialized for when the user enters the correct answer
+ */
 public class SuccessSpeechResult extends SpeechResult{
 
     @Override
+    /**
+     * Initializes result screen for correct answer
+     */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_speech_result);
