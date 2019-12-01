018480a88a0b59440974802e3ba0c6342d2513a7
Guanjie <guanjie.wang@mail.utoronto.ca>
2019-11-30 23:49:49 -0500

    Introduce Parent class UserPopUpActivity for LoginActivity and RegisterActivity

--- phase2/PoliticGame/app/src/main/java/com/example/politicgame/PopUpActivity.java
+++ phase2/PoliticGame/app/src/main/java/com/example/politicgame/PopUpActivity.java
@@ -1,31 +1,47 @@
 package com.example.politicgame;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Observer;
 
 import com.example.politicgame.Application.PoliticGameApp;
+import com.example.politicgame.UserActivity.FormState;
+import com.example.politicgame.UserActivity.LoginActivity.LoggedInUserView;
+import com.example.politicgame.UserActivity.LoginActivity.ViewModelResult;
+import com.example.politicgame.UserActivity.UserViewModel;
 
 public abstract class PopUpActivity extends FragmentActivity {
 
-    /** The application. */
-    protected PoliticGameApp app;
+  /** The application. */
+  protected PoliticGameApp app;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
+    DisplayMetrics dm = new DisplayMetrics();
+    getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
+    int width = dm.widthPixels;
+    int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.8), (int)(height*.8));
+    getWindow().setLayout((int) (width * .8), (int) (height * .8));
 
-        app = (PoliticGameApp) getApplication();
-
-        setTheme(R.style.PopUp);
-    }
+    app = (PoliticGameApp) getApplication();
+    setTheme(R.style.PopUp);
+  }
 }
