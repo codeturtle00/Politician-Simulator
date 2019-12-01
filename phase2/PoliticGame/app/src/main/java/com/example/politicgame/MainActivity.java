3c856123269de8fdd360bbefc2685bb21866a409
Jacky Yang <yacky.yang@mail.utoronto.ca>
2019-12-01 03:02:42 -0500

    comments/code cleanup

--- phase2/PoliticGame/app/src/main/java/com/example/politicgame/MainActivity.java
+++ phase2/PoliticGame/app/src/main/java/com/example/politicgame/MainActivity.java
@@ -6,22 +6,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.example.politicgame.Games.BabyGame.BabyGameInstruction;
 import com.example.politicgame.Leaderboard.LeaderBoardActivity;
 import com.example.politicgame.LoadCharacter.LoadCharacterActivity;
-import com.example.politicgame.Pausing.PauseActivity;
 import com.example.politicgame.UserActivity.LoginActivity.LoginActivity;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends GameActivity {
 
   boolean theme;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,106 +24,94 @@ public class MainActivity extends GameActivity {
     theme = app.isThemeBlue();
     setContentView(R.layout.activity_main);
 
+    // Animate Trump
     ImageView trumpIMG = findViewById(R.id.trump);
     Animation animated_trump = AnimationUtils.loadAnimation(this, R.anim.animated_trump);
     trumpIMG.startAnimation(animated_trump);
 
+    // Animate Title
     ImageView titleIMG = findViewById(R.id.gameTitle);
     Animation animated_title = AnimationUtils.loadAnimation(this, R.anim.animated_title);
     titleIMG.startAnimation(animated_title);
 
-    //Generate Login button
-    loginButton();
-
-    //Generate Select Characters button
-    selectCharacterButton();
-
-    //Generate Leaderboard button
-    leaderboardButton();
-
-    // Settings button, opens the settings menu
-    final ImageButton settingButton = findViewById(R.id.settings);
-    settingButton.setOnClickListener(
-        new View.OnClickListener() {
-          public void onClick(View v) {
-            // Code here executes on main thread after user presses button
-            openSettings();
-          }
-        });
+    generateLoginButton((TextView) findViewById(R.id.login));
+    generateSelectCharacterButton(findViewById(R.id.play));
+    generateLeaderboardButton(findViewById(R.id.leaderBoard));
+    generateSettingsButton(findViewById(R.id.settings));
   }
 
   @Override
   protected void onResume() {
     super.onResume();
     // Reload buttons that depend on if user is logged in
-    selectCharacterButton();
-    loginButton();
-    // If theme has changed, reload
-//    if (theme != app.isThemeBlue()) recreate();
+    generateLoginButton((TextView) findViewById(R.id.login));
+    generateSelectCharacterButton(findViewById(R.id.play));
   }
 
-  private void leaderboardButton() {
-    final Button boardButton = findViewById(R.id.leaderBoard);
+  private void generateLeaderboardButton(View boardButton) {
     boardButton.setOnClickListener(
         new View.OnClickListener() {
           public void onClick(View v) {
-            // Code here executes on main thread after user presses button
             openLeaderBoard();
           }
         });
   }
 
-  private void selectCharacterButton() {
-    final Button selectCharactersButton = findViewById(R.id.play);
-
+  private void generateSelectCharacterButton(View selectCharactersButton) {
     if (app.getCurrentUser() == null) {
       selectCharactersButton.setEnabled(false);
-    }
-    else {
+    } else {
       selectCharactersButton.setEnabled(true);
-      selectCharactersButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-          openLoadCharacter();
-        }
-      });
+      selectCharactersButton.setOnClickListener(
+          new View.OnClickListener() {
+            public void onClick(View v) {
+              openLoadCharacter();
+            }
+          });
     }
   }
 
-  private void loginButton() {
-    final Button loginButton = findViewById(R.id.login);
+  private void generateLoginButton(TextView loginButton) {
 
+    // Login Button changes to Sign Out if already logged in
     if (app.getCurrentUser() == null) {
       loginButton.setText(getString(R.string.login));
       loginButton.setOnClickListener(
-              new View.OnClickListener() {
-                public void onClick(View v) {
-                  // Code here executes on main thread after user presses button
-                  openLoginPage();
-                }
-              });
+          new View.OnClickListener() {
+            public void onClick(View v) {
+              openLoginPage();
+            }
+          });
     } else {
       loginButton.setText(getString(R.string.sign_out));
       loginButton.setOnClickListener(
-              new View.OnClickListener() {
-                public void onClick(View v) {
-                  app.setCurrentUser(null);
-                  // Reload necessary buttons
-                  selectCharacterButton();
-                  loginButton();
-                }
-              });
+          new View.OnClickListener() {
+            public void onClick(View v) {
+              app.setCurrentUser(null);
+              // Reload necessary buttons
+              generateLoginButton((TextView) findViewById(R.id.login));
+              generateSelectCharacterButton(findViewById(R.id.play));
+            }
+          });
     }
   }
 
+  private void generateSettingsButton(View settingButton) {
+    settingButton.setOnClickListener(
+        new View.OnClickListener() {
+          public void onClick(View v) {
+            openSettings();
+          }
+        });
+  }
+
   public void openLoginPage() {
-    /** Opens the login page */
     Intent loginPageIntent = new Intent(this, LoginActivity.class);
     startActivity(loginPageIntent);
-    overridePendingTransition(R.anim.slide_up, R.anim.slide_up);
+    overridePendingTransition(R.anim.slide_up, 0);
   }
 
   public void openLeaderBoard() {
-    /** Opens the leaderboard screen */
     Intent switchBoardIntent = new Intent(this, LeaderBoardActivity.class);
     switchBoardIntent.putExtra("BoardType", "Election Mode");
     startActivity(switchBoardIntent);
@@ -136,33 +119,36 @@ public class MainActivity extends GameActivity {
   }
 
   public void openSettings() {
-    /** Open the settings menu */
-      Intent pauseMenuIntent = new Intent(this, SettingsActivity.class);
-      startActivityForResult(pauseMenuIntent, 2);
+    Intent pauseMenuIntent = new Intent(this, SettingsActivity.class);
+    startActivityForResult(pauseMenuIntent, 2);
   }
 
-  public void openLoadCharacter () {
+  public void openLoadCharacter() {
     Intent loadCharacters = new Intent(this, LoadCharacterActivity.class);
     startActivity(loadCharacters);
   }
 
+  /**
+   * Add on to GameActivity's onActivityResult. We add requestCode 2 for settings
+   * to tell Main Menu user has changed themes.
+   */
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
-      if (requestCode == 2) {
-          if (resultCode == RESULT_OK) {
-              int userInput = data.getIntExtra("result", 0);
+    if (requestCode == 2) {
+      if (resultCode == RESULT_OK) {
+        int userInput = data.getIntExtra("result", 0);
 
-              if (userInput == GameActivity.REFRESH_BG) {
-                  Log.i("onActivityResult", "Refreshing background activity");
-                  overridePendingTransition(0, 0);
-                  recreate();
-                  overridePendingTransition(0, 0);
-                  openSettings();
-              }
-          } else {
-              Log.i("Result Code", "Result code is " + resultCode);
-          }
-      }
+        if (userInput == GameActivity.REFRESH_BG) {
+          Log.i("onActivityResult", "Refreshing background activity");
+          overridePendingTransition(0, 0);
+          recreate();
+          overridePendingTransition(0, 0);
+          openSettings();
         }
+      } else {
+        Log.i("Result Code", "Result code is " + resultCode);
+      }
+    }
+  }
 }
